0b3bae2fa6e097fbab692a659c7a36b49b2bf59c
Kanaricc <46213939+Kanaricc@users.noreply.github.com>
2021-08-16 19:49:32 +0800

    chores: remove twitter link

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: '',
   seo: {
     keywords: ['chencheng','chengchen','algorithm','math'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
